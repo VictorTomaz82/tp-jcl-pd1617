77750bf78b4722a24a5f28239cba18baa29e5422
victor <victor@CyberClient>
2016-12-28 19:06:06 +0000

    Change from userId to username Denunce function -> denounceItem & denounceUser

--- src/tpserver/ButlerRemote.java
+++ src/tpserver/ButlerRemote.java
@@ -18,31 +18,33 @@ public interface ButlerRemote {
 
     public ArrayList<String> askReactivation(String username, String password);
 
-    public ArrayList<String> changePassword(String username, String password, String confirmPassword);
+    public ArrayList<String> changePassword(String username, String password, String newPassword, String confirmPassword);
 
     public ArrayList<String> messageUser(String senderId, String recipientId, String title, String body, Date time);
 
-    public ArrayList<String> doSale(String name, String description, String sellerId, int startPrice);
+    public ArrayList<String> doSale(String sellerUsername, String itemName, String description, int startPrice, int buyout);
 
     public ArrayList<String> follow(String itemId);
 
-    public ArrayList<String> doBid(String itemId);
+    public ArrayList<String> doBid(String itemId, int bid);
 
-    public ArrayList<String> denunce(String userId, String itemId, String motive);
+    public ArrayList<String> denunceItem(String itemId, String motive);
+    
+    public ArrayList<String> denunceUser(String username, String motive);
 
-    public ArrayList<String> addBalance(int money);
+    public ArrayList<String> addBalance(String username, int money);
 
     public ArrayList<String> payItem(String itemId);
 
-    public ArrayList<String> askSuspension(String userId, String motive);
+    public ArrayList<String> askSuspension(String username, String motive);
 
-    public ArrayList<String> unactivate(String userId);
+//    public ArrayList<String> unactivate(String userId);
 
-    public ArrayList<String> suspendUser(String userId, String motive);
+    public ArrayList<String> suspendUser(String username, String motive);
 
-    public ArrayList<String> reactivateUser(String userId);
+    public ArrayList<String> reactivateUser(String username);
 
-    public ArrayList<String> seeUser(String userId);
+    public ArrayList<String> seeUser(String username);
 
     public ArrayList<String> seeItem(String itemId);
 
