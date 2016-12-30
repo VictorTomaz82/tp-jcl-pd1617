a2f358e4e95bbfe391823797724104fe115205bc
victor <victor@CyberClient>
2016-12-30 18:23:52 +0000

    update on remote butler

--- src/tpserver/ButlerRemote.java
+++ src/tpserver/ButlerRemote.java
@@ -6,17 +6,18 @@ import javax.ejb.Remote;
 
 @Remote
 public interface ButlerRemote {
-    
+
     public ArrayList<String> login(String username, String password);
-    
+    public void logout();
+
     public String getCurrentUsername();
-    
+
     // these functions will call upon the usertype for user previleges
     public ArrayList<String> seeNews();
 
     public ArrayList<String> seeLastThree();
 
-    public ArrayList<String> askAccess(String username, String password);
+    public ArrayList<String> askAccess(String username, String password, String name, String address);
 
     public ArrayList<String> askReactivation(String username, String password);
 
@@ -31,7 +32,7 @@ public interface ButlerRemote {
     public ArrayList<String> doBid(String itemId, int bid);
 
     public ArrayList<String> denunceItem(String itemId, String motive);
-    
+
     public ArrayList<String> denunceUser(String username, String motive);
 
     public ArrayList<String> addBalance(String username, int money);
@@ -41,7 +42,6 @@ public interface ButlerRemote {
     public ArrayList<String> askSuspension(String username, String motive);
 
 //    public ArrayList<String> unactivate(String userId);
-
     public ArrayList<String> suspendUser(String username, String motive);
 
     public ArrayList<String> reactivateUser(String username);
