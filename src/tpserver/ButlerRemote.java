0c0abc3409a37797863c1e2714b37754ba767834
korshar <andre_pinho_93@outlook.com>
2016-12-31 15:16:11 +0000

    Added moar functions and more lame things update

--- src/tpserver/ButlerRemote.java
+++ src/tpserver/ButlerRemote.java
@@ -31,7 +31,7 @@ public interface ButlerRemote {
 
     public ArrayList<String> messageUser(String senderId, String recipientId, String title, String body, Date time);
 
-    public ArrayList<String> doSale(String sellerUsername, String itemName, String description, int startPrice, int buyout);
+    public ArrayList<String> doSale(String sellerUsername, String itemName, String category, String description, int startPrice, int buyout);
         
     public ArrayList<String> viewUserItemsList();
     
