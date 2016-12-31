906c1ab52d9840e17a8707901042aa17d7367dd4
korshar <andre_pinho_93@outlook.com>
2016-12-31 04:01:28 +0000

    Added moar functions and more lame things

--- src/tpserver/ButlerRemote.java
+++ src/tpserver/ButlerRemote.java
@@ -8,7 +8,7 @@ import javax.ejb.Remote;
 public interface ButlerRemote {
 
     public ArrayList<String> login(String username, String password);
-    public void logout();
+    public ArrayList<String> logout();
 
     public String getCurrentUsername();
 
@@ -22,11 +22,27 @@ public interface ButlerRemote {
     public ArrayList<String> askReactivation(String username, String password);
 
     public ArrayList<String> changePassword(String username, String password, String newPassword, String confirmPassword);
+    
+    public ArrayList<String> changeUserInformation(String name,String address);
+    
+    public ArrayList<String> viewMessageList();
+    
+    public ArrayList<String> viewMessage(String messageId);
 
     public ArrayList<String> messageUser(String senderId, String recipientId, String title, String body, Date time);
 
     public ArrayList<String> doSale(String sellerUsername, String itemName, String description, int startPrice, int buyout);
-
+        
+    public ArrayList<String> viewUserItemsList();
+    
+    public ArrayList<String> viewSellingItemList();
+    
+    public ArrayList<String> viewBiddedItemList();
+    
+    public ArrayList<String> viewWonItemList();
+    
+    public ArrayList<String> viewFollowedItemList();
+    
     public ArrayList<String> follow(String itemId);
 
     public ArrayList<String> doBid(String itemId, int bid);
@@ -34,6 +50,8 @@ public interface ButlerRemote {
     public ArrayList<String> denunceItem(String itemId, String motive);
 
     public ArrayList<String> denunceUser(String username, String motive);
+    
+    public ArrayList<String> viewUserBalance();
 
     public ArrayList<String> addBalance(String username, int money);
 
@@ -42,13 +60,23 @@ public interface ButlerRemote {
     public ArrayList<String> askSuspension(String username, String motive);
 
 //    public ArrayList<String> unactivate(String userId);
+    public ArrayList<String> viewDenunceList();
+    
     public ArrayList<String> suspendUser(String username, String motive);
+    
+    public ArrayList<String> itemRemove(String itemId);
 
     public ArrayList<String> reactivateUser(String username);
+    
+    public ArrayList<String> viewUserList(); 
 
     public ArrayList<String> seeUser(String username);
+    
+    public ArrayList<String> viewItemList();
 
     public ArrayList<String> seeItem(String itemId);
+    
+    public ArrayList<String> viewCategoryList();
 
     public ArrayList<String> addCategory(String name, String description);
 
